5fbdc48c188c99555ca0ecbbcafb14ccd90a1779
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2019-04-26 21:16:15 +0200

    Initial Commit

---
+++ FlickrSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrSampleApp
+//
+//  Created by Andrii Kuzminskyi on 26/04/2019.
+//  Copyright © 2019 akuzminskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
