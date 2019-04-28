90436f25de48a4c41adf72431e1bf02c6310f2bc
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2019-04-28 20:51:04 +0200

    Added basic UI - unfinished

--- FlickrSampleApp/Screen/PhotoSearch/View/PhotoSearchViewController.swift
+++ FlickrSampleApp/Screen/PhotoSearch/View/PhotoSearchViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 final class PhotoSearchViewController: UIViewController {
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var collectionView: UICollectionView!
     var output: PhotoSearchViewOutput?
     
     override func viewDidLoad() {
@@ -17,8 +19,30 @@ final class PhotoSearchViewController: UIViewController {
     }
 }
 
+extension PhotoSearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell(frame: .zero)
+    }
+}
+
+extension PhotoSearchViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+
+    }
+}
+
 extension PhotoSearchViewController: PhotoSearchViewInput {
     func showSearchingIndicator(_ isSearching: Bool) {
+        if isSearching {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        collectionView.isUserInteractionEnabled = !isSearching
     }
 }
 
