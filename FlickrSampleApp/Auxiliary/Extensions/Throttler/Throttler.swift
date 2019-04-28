2e73c9567b364f41ca4bf8e947106cc6129668d1
Andrii Kuzminskyi <andrii.kuzminskyi@gmail.com>
2019-04-28 17:59:49 +0200

    Added Throttler for search

---
+++ FlickrSampleApp/Auxiliary/Extensions/Throttler/Throttler.swift
@@ -0,0 +1,30 @@
+//
+//  Throttler.swift
+//  FlickrSampleApp
+//
+//  Created by Andrii Kuzminskyi on 28/04/2019.
+//  Copyright © 2019 akuzminskyi. All rights reserved.
+//
+
+import Foundation
+
+final class Throttler {
+    private var workItem: DispatchWorkItem = DispatchWorkItem(block: { })
+    private let queue: DispatchQueue
+    private let timeout: TimeInterval
+
+    init(timeout: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
+        self.timeout = timeout
+        self.queue = queue
+    }
+
+    func throttle(_ block: @escaping () -> Void) {
+        workItem.cancel()
+
+        workItem = DispatchWorkItem() {
+            block()
+        }
+
+        queue.asyncAfter(deadline: .now() + timeout, execute: workItem)
+    }
+}
